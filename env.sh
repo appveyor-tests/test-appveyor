dbfed5ed0538a20fc9f270a01effdbc4107aaf8e
Vasily Pleshakov <wasa@yandex.ru>
2019-06-19 00:04:43 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,10 +2,15 @@
 
 firstArg=${1:-DefaultValue}
 firstArg2=${1-DefaultValue}
+firstArg3=${1:=DefaultValue}
 
-echo First script argument has a value of $firstArg
-echo Alternative syntax, has a value of $firstArg2
+echo "First script argument has a value of $firstArg"
+echo "Alternative syntax, has a value of $firstArg2"
+echo "Alternative syntax, has a value of $firstArg3"
 
-echo "---"
-echo "DefaultValue"
-echo "---"
+A=${1:-DefValue}
+B=${1-DefValue}
+
+echo First script argument has a value of $A
+echo Alternative syntax, has a value of $B
+
